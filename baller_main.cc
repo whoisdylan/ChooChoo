2f6fa3efc4ad2cf2a120106bc20512ba07b6a2ea
Dylan Koenig <koenigup@gmail.com>
2014-10-30 00:35:13 -0700

    change to .cc extensions, start factoring out ball

--- baller_main.cc
+++ baller_main.cc
@@ -1,32 +1,27 @@
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
 #include <vector>
 #include <math.h>
 #include <iostream>
+#include "Ball.hh"
 
 #define PI 3.14159265
 
 using namespace std;
 using namespace sf;
 
-constexpr int windowWidth{800}, windowHeight{600};
-constexpr float ballRadius{10.f};
-
-struct Ball {
-	CircleShape shape;
-  RectangleShape aimer;
-	Ball (float mX, float mY) {
-		shape.setPosition(mX, mY);
-		shape.setRadius(ballRadius);
-		shape.setFillColor(Color::Green);
-		shape.setOrigin(ballRadius, ballRadius);
-    aimer.setPosition(mX, mY);
-    aimer.setSize(Vector2f(50, 4));
-    aimer.setFillColor(Color::Cyan);
-		aimer.setOrigin(0, 2);
-	}
-
-};
+// struct Ball {
+// 	CircleShape shape;
+//   RectangleShape aimer;
+// 	Ball (float mX, float mY) {
+// 		shape.setPosition(mX, mY);
+// 		shape.setRadius(ballRadius);
+// 		shape.setFillColor(Color::Green);
+// 		shape.setOrigin(ballRadius, ballRadius);
+//     aimer.setPosition(mX, mY);
+//     aimer.setSize(Vector2f(50, 4));
+//     aimer.setFillColor(Color::Cyan);
+// 		aimer.setOrigin(0, 2);
+// 	}
+// };
 
 void handleMovement(vector<int> &velocity) {
 	if (Keyboard::isKeyPressed(Keyboard::Key::Left)) {
