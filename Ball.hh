e2e44deaa31c1b2d1a3c552c99aa07737d35560b
Dylan Koenig <koenigup@gmail.com>
2014-10-30 01:50:08 -0700

    ball: move constructor out of .hh

--- Ball.hh
+++ Ball.hh
@@ -14,16 +14,7 @@ class Ball {
   public:
     CircleShape shape;
     RectangleShape aimer;
-    Ball (float mX, float mY) {
-      shape.setPosition(mX, mY);
-      shape.setRadius(ballRadius);
-      shape.setFillColor(Color::Green);
-      shape.setOrigin(ballRadius, ballRadius);
-      aimer.setPosition(mX, mY);
-      aimer.setSize(Vector2f(50, 4));
-      aimer.setFillColor(Color::Cyan);
-      aimer.setOrigin(0, 2);
-    }
+    Ball (float, float);
 };
 
 #endif
